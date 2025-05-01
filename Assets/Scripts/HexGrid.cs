bed9f13d41a1bacf26fb06ed158e995e4dee4d21
Mark Roberts <mmmcaffeine@gmail.com>
2025-05-01 19:21:58 +0100

    Add HexGrid class and list of things it eventually needs to do

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    // TODO Add properties for grid size, hex size, and hex prefab
+    // TODO Create a grid of hexes
+    // TODO Store the individual tiles in an array
+    // TODO Methods to get, change, add, and remove tiles
+    // TODO Gizmo for drawing the grid in the editor
+}
