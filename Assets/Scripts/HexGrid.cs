19cb8aaa92287849c6bbb9a19d21ee457963827f
Mark Roberts <mmmcaffeine@gmail.com>
2025-05-02 09:32:16 +0100

    Add properties to HexGrid allowing a grid to be defined in Unity editor

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
-    // TODO Add properties for grid size, hex size, and hex prefab
     // TODO Create a grid of hexes
     // TODO Store the individual tiles in an array
     // TODO Methods to get, change, add, and remove tiles
     // TODO Gizmo for drawing the grid in the editor
+
+    [field: SerializeField] public HexOrientation Orientation { get; private set; }
+    [field: SerializeField] public int Width { get; private set; }
+    [field: SerializeField] public int Height { get; private set; }
+    [field: SerializeField] public float HexSize { get; private set; }
+    [field: SerializeField] public GameObject HexPrefab { get; private set; }
 }
