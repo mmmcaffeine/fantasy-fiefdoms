35eaa680b5c6a3cb9aef52be66d5fb05a089cb07
Mark Roberts <mmmcaffeine@gmail.com>
2025-05-02 09:36:55 +0100

    Add OnDrawGizmos to draw the defined HexGrid in the Unity editor

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -5,11 +5,27 @@ public class HexGrid : MonoBehaviour
     // TODO Create a grid of hexes
     // TODO Store the individual tiles in an array
     // TODO Methods to get, change, add, and remove tiles
-    // TODO Gizmo for drawing the grid in the editor
 
     [field: SerializeField] public HexOrientation Orientation { get; private set; }
     [field: SerializeField] public int Width { get; private set; }
     [field: SerializeField] public int Height { get; private set; }
     [field: SerializeField] public float HexSize { get; private set; }
     [field: SerializeField] public GameObject HexPrefab { get; private set; }
+
+    private void OnDrawGizmos()
+    {
+        for (var z = 0; z < Height; z++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var centerPosition = HexMetrics.Center(HexSize, x, z, Orientation) + transform.position;
+                var corners = HexMetrics.Corners(HexSize, Orientation);
+
+                for (var s = 0; s < corners.Length; s++)
+                {
+                    Gizmos.DrawLine(centerPosition + corners[s % 6], centerPosition + corners[(s + 1) % 6]);
+                }
+            }
+        }
+    }
 }
